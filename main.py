4f99b053ca214a611c69f6dc3e897148d4db9906
Heizenber <59764703+Heizenber@users.noreply.github.com>
2021-10-30 22:51:27 +0300

    Completed Spot Class

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.boxlayout import BoxLayout
 from kivy.utils import get_color_from_hex
+from kivy.properties import ListProperty
 from colors import *
 from queue import PriorityQueue
 
@@ -11,7 +12,45 @@ Builder.load_file("interface.kv")
 
 
 class Spot(Widget):
-    pass
+    color = ListProperty(*get_color_from_hex(WHITE))
+    startPresent = False
+    endPresent = False
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def on_touch_down(self, touch):
+        x, y = touch.pos
+        if self.collide_point(x, y):
+            if not Spot.startPresent and self.color != BLUE:
+                self.color = RED
+                Spot.startPresent = True
+
+            elif not Spot.endPresent and self.color != RED:
+                self.color = BLUE
+                Spot.endPresent = True
+            else:
+                if self.color == BLACK:
+                    self.color = WHITE
+                elif self.color == RED:
+                    self.color = WHITE
+                    Spot.startPresent = False
+                elif self.color == BLUE:
+                    self.color = WHITE
+                    Spot.endPresent = False
+                else:
+                    self.color = BLACK
+            return
+        else:
+            return super().on_touch_down(touch)
+
+    def on_touch_move(self, touch):
+        x, y = touch.pos
+        if self.collide_point(x, y):
+            self.color = BLACK
+            return
+        else:
+            return super().on_touch_down(touch)
 
 
 class Grid(GridLayout):
