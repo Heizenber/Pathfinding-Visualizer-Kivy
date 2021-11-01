9ffb90ccaf2948fb0523f65952f4481b1dc3be2d
Heizenber <59764703+Heizenber@users.noreply.github.com>
2021-11-01 16:19:27 +0200

    Added algo files

---
+++ algorithms/DijkstraAlgorithm.py
@@ -0,0 +1,5 @@
+from colors import *
+
+
+def dijkstraAlgo():
+    pass
--- main.py
+++ main.py
@@ -2,13 +2,20 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.gridlayout import GridLayout
+from algorithms.AStarAlgorithm import aStarAlgo
+from algorithms.DijkstraAlgorithm import dijkstraAlgo
 from kivy.uix.boxlayout import BoxLayout
 from kivy.utils import get_color_from_hex
 from kivy.properties import ListProperty
+from kivy.core.window import Window
+from threading import Thread
 from colors import *
 from queue import PriorityQueue
 
 Builder.load_file("interface.kv")
+
+
+algorithms = {"A* Algorithm": aStarAlgo, "Dijkstra": dijkstraAlgo}
 
 
 class Spot(Widget):
@@ -46,7 +53,7 @@ class Spot(Widget):
 
     def on_touch_move(self, touch):
         x, y = touch.pos
-        if self.collide_point(x, y):
+        if self.collide_point(x, y) and self.color == WHITE:
             self.color = BLACK
             return
         else:
@@ -54,7 +61,24 @@ class Spot(Widget):
 
 
 class Grid(GridLayout):
-    pass
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self.rows = 34
+        self.cols = 41
+        self.spacing = 1
+        self.grid = [self.add_widget(Spot()) for _ in range(self.rows * self.cols)]
+        self.edge_y = Window.height / self.rows
+        self.edge_x = Window.width / self.cols
+        self.current = None
+        self.open_set = []
+        self.closed_set = []
+        self.end = None
+
+    def start(self):
+        if Spot.startPresent and Spot.endPresent:
+            Thread(
+                target=algorithms[Interface.root.ids.text], args=(), daemon=True
+            ).start()
 
 
 class Interface(BoxLayout):
