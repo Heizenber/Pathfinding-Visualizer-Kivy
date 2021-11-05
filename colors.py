44f7c135f161419e8abc2938f7ca6cc2c54c3301
Heizenber <59764703+Heizenber@users.noreply.github.com>
2021-11-05 15:02:59 +0200

    made a few changes to Node class and colors file

--- colors.py
+++ colors.py
@@ -1,11 +1,13 @@
-DARK_GRAY = "#65696B"
-LIGHT_GRAY = "#C4C5BF"
-BLUE = "#0CA8F6"
-DARK_BLUE = "#4204CC"
-RED = "#F22810"
-YELLOW = "#F7E806"
-LIGHT_GREEN = "#05F50E"
-PURPLE = "#BF01FB"
-WHITE = "#ffffff"
-BLACK = "#000000"
-GREEN = "##28d821"
+from kivy.utils import get_color_from_hex
+
+DARK_GRAY = get_color_from_hex("#65696B")
+LIGHT_GRAY = get_color_from_hex("#C4C5BF")
+BLUE = get_color_from_hex("#0CA8F6")
+DARK_BLUE = get_color_from_hex("#4204CC")
+RED = get_color_from_hex("#F22810")
+YELLOW = get_color_from_hex("#F7E806")
+LIGHT_GREEN = get_color_from_hex("#05F50E")
+PURPLE = get_color_from_hex("#BF01FB")
+WHITE = get_color_from_hex("#ffffff")
+BLACK = get_color_from_hex("#000000")
+GREEN = get_color_from_hex("#28d821")
--- main.py
+++ main.py
@@ -6,13 +6,12 @@ from algorithms.AStarAlgorithm import aStarAlgo
 from algorithms.DijkstraAlgorithm import dijkstraAlgo
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
-from kivy.utils import get_color_from_hex, get_hex_from_color
 from kivy.properties import ListProperty
 from kivy.core.window import Window
 from kivy.uix.popup import Popup
 from threading import Thread
 from colors import *
-from queue import PriorityQueue
+
 
 Builder.load_file("interface.kv")
 
@@ -20,33 +19,42 @@ Builder.load_file("interface.kv")
 algorithms = {"A* Algorithm": aStarAlgo, "Dijkstra": dijkstraAlgo}
 
 
-class Spot(Widget):
-    color = ListProperty(get_color_from_hex(WHITE))
+class Node(Widget):
+    color = ListProperty(WHITE)
     startPresent = False
     endPresent = False
 
-    def __init__(self, **kwargs):
+    def __init__(self, row, col, **kwargs):
         super().__init__(**kwargs)
+        self.id = str(row) + "-" + str(col)
+        self.row = row
+        self.col = col
+        self.distanceFromStart = float("inf")
+        self.estimatedDistanceToEnd = float("inf")
+        self.cameFrom = None
 
     def on_touch_down(self, touch):
         x, y = touch.pos
         if self.collide_point(x, y):
-            if not Spot.startPresent and get_hex_from_color(self.color) != BLUE:
+            if not Node.startPresent and get_hex_from_color(self.color) != BLUE:
                 self.color = get_color_from_hex(RED)
-                Spot.startPresent = True
+                Node.startPresent = True
 
-            elif not Spot.endPresent and get_hex_from_color(self.color) != RED:
+            elif not Node.endPresent and get_hex_from_color(self.color) != RED:
                 self.color = get_color_from_hex(BLUE)
-                Spot.endPresent = True
+                Node.endPresent = True
             else:
-                if get_hex_from_color(self.color) == BLACK:
-                    self.color = get_color_from_hex(WHITE)
-                elif get_hex_from_color(self.color) == RED:
-                    self.color = get_color_from_hex(WHITE)
-                    Spot.startPresent = False
-                elif get_hex_from_color(self.color) == BLUE:
-                    self.color = get_color_from_hex(WHITE)
-                    Spot.endPresent = False
+                if touch.button == "right":
+                    if get_hex_from_color(self.color) == BLACK:
+                        self.color = get_color_from_hex(WHITE)
+                # if get_hex_from_color(self.color) == BLACK:
+                #     self.color = get_color_from_hex(WHITE)
+                # elif get_hex_from_color(self.color) == RED:
+                #     self.color = get_color_from_hex(WHITE)
+                #     Node.startPresent = False
+                # elif get_hex_from_color(self.color) == BLUE:
+                #     self.color = get_color_from_hex(WHITE)
+                #     Node.endPresent = False
                 else:
                     if (
                         get_hex_from_color(self.color) != RED
@@ -76,16 +84,19 @@ class Grid(GridLayout):
         self.rows = 34
         self.cols = 41
         self.spacing = 1
-        self.grid = [self.add_widget(Spot()) for _ in range(self.rows * self.cols)]
-        self.edge_y = Window.height / self.rows
-        self.edge_x = Window.width / self.cols
-        self.current = None
+        self.generate_grid()
         self.open_set = []
         self.closed_set = []
-        self.end = None
+
+    def generate_grid(self):
+        self.grid = [
+            self.add_widget(Node(i, j))
+            for j in range(self.cols)
+            for i in range(self.rows)
+        ]
 
     def start(self):
-        if Spot.startPresent and Spot.endPresent:
+        if Node.startPresent and Node.endPresent:
             Thread(
                 target=algorithms[Interface.root.ids.text], args=(), daemon=True
             ).start()
@@ -111,4 +122,5 @@ class PathfindingApp(App):
 
 
 if __name__ == "__main__":
+    Window.clearcolor = get_color_from_hex(LIGHT_GRAY)
     PathfindingApp().run()
